7b5dabcfa37d12b794e5f89748280dd82363a9cf
Tristan Palmer <palmertt@mail.uc.edu>
2020-09-04 13:52:43 -0400

    added University table to admin page, added University of Cincinnati to database

--- college_dreaming/settings.py
+++ college_dreaming/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'universities.apps.UniversitiesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- universities/models.py
+++ universities/models.py
@@ -16,3 +16,6 @@ class University(models.Model):
     required_sat_score = models.CharField(max_length=4)
     weather = models.CharField(max_length=10)
     most_popular_major = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.name
