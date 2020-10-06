d9dc2384e215c26e199e70add7e53045a17ff748
Tristan Palmer <palmertt@mail.uc.edu>
2020-10-06 18:50:05 -0400

    Added contact page layout and functionality.

--- universities/models.py
+++ universities/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
 class University(models.Model):
     name = models.CharField(max_length=150)
     population = models.CharField(max_length=50)
-    amount_of_clubs = models.CharField(max_length=10) 
+    amount_of_clubs = models.CharField(max_length=10)
     known_for = models.CharField(max_length=100)
     public_or_private = models.CharField(max_length=25)
     in_state_tuition = models.CharField(max_length=8)
@@ -19,6 +20,7 @@ class University(models.Model):
 
     def __str__(self):
         return self.name
+
 
 class NearbyRestaurant(models.Model):
     university = models.ForeignKey(University, on_delete=models.CASCADE)
---
+++ universities/static/universities/js/main.js
@@ -0,0 +1,16 @@
+// Contact Page information
+const sendEmail = document.getElementById('contact');
+const emailSender = document.getElementById('emailInput');
+const emailContent = document.getElementById('messageContent');
+const subject = document.getElementById('subject');
+const emailRecipient = 'seniordesign908@gmail.com'
+
+sendEmail.addEventListener('click', function () {
+
+    let mailto_link = 'mailto:' + emailRecipient + '?subject=' + 'Email From '+ emailSender.value + ': ' + subject.value + '&body=' + emailContent.value;
+
+    window = window.open(mailto_link, 'emailWindow');
+    if (window && window.open && !window.closed) {
+        window.close();
+    }
+});
