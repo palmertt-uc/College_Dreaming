f61c7ebbbfc1cc559e1a8a6229f551e56bde24be
Tristan Palmer <palmertt@mail.uc.edu>
2020-10-11 21:38:49 -0400

    Changed About page, fixed URLs displaying wrong routes, changed the homepages link in HTML base file

--- universities/urls.py
+++ universities/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='home'),
     path('universities/', views.universities, name='universities'),
     path('search/', views.search, name='search'),
     path('about/', views.about, name='about'),
