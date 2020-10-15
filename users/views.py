7fbaaf627c0d336f41abe961fd243497a7e8a330
Tristan Palmer <palmertt@mail.uc.edu>
2020-10-15 17:16:17 -0400

    Added more information for the profile page. Added a profile model, that links directly with the Users model. Added function for updating user profile.

--- users/views.py
+++ users/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 from django.contrib.auth.decorators import login_required
 
 
@@ -8,6 +8,7 @@ from django.contrib.auth.decorators import login_required
 def register(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
+
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
@@ -20,4 +21,22 @@ def register(request):
 
 @login_required
 def profile(request):
-    return render(request, 'users/profile.html')
+    if request.method == 'POST':
+        user_update = UserUpdateForm(request.POST, instance=request.user)
+        profile_update = ProfileUpdateForm(request.POST, instance=request.user.profile)
+
+        if user_update.is_valid() and profile_update.is_valid():
+            user_update.save()
+            profile_update.save()
+            messages.success(request, 'Account Updated')
+            return redirect('profile')
+    else:
+        user_update = UserUpdateForm(instance=request.user)
+        profile_update = ProfileUpdateForm(instance=request.user)
+
+    context = {
+        'user_update': user_update,
+        'profile_update': profile_update
+    }
+
+    return render(request, 'users/profile.html', context)
