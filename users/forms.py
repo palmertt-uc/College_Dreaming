3cee92ddcf9a006b4b6e6b3e6038d2be09f90bee
Tristan Palmer <palmertt@mail.uc.edu>
2020-10-20 21:08:07 -0400

    Added more columns to the Profiles table, added more content in the profile.html file.

--- users/forms.py
+++ users/forms.py
@@ -24,4 +24,4 @@ class ProfileUpdateForm(forms.ModelForm):
 
     class Meta:
         model = Profile
-        fields = ['bio']
+        fields = ['first_name', 'last_name', 'gpa', 'sat', 'act', 'bio']
--- users/models.py
+++ users/models.py
@@ -5,6 +5,11 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=25, blank=True, default='')
+    last_name = models.CharField(max_length=25, blank=True, default='')
+    gpa = models.CharField(max_length=5, blank=True, default='')
+    act = models.CharField(max_length=2, blank=True, default='')
+    sat = models.CharField(max_length=4, blank=True, default='')
     bio = models.TextField(max_length=1000, blank=True, default='')
 
     def __str__(self):
