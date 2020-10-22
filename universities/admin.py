92d304fcf4e02e55777c73de2ce5cd607cd4f396
devinwilliams <devinqwilliams13@gmail.com>
2020-10-22 12:27:15 -0400

    changes nav bar and added new models

--- universities/admin.py
+++ universities/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import University, NearbyRestaurant
+from .models import Institutions, Cities, Zipcodes
 
 # Register your models here.
-admin.site.register(University)
-admin.site.register(NearbyRestaurant)
+admin.site.register(Institutions)
+admin.site.register(Cities)
+admin.site.register(Zipcodes)
--- universities/models.py
+++ universities/models.py
@@ -2,32 +2,55 @@ from django.db import models
 
 
 # Create your models here.
-class University(models.Model):
-    name = models.CharField(max_length=150)
-    population = models.CharField(max_length=50)
-    amount_of_clubs = models.CharField(max_length=10)
-    known_for = models.CharField(max_length=100)
-    public_or_private = models.CharField(max_length=25)
-    in_state_tuition = models.CharField(max_length=8)
-    out_state_tuition = models.CharField(max_length=8)
-    university_image = models.FileField()
-    location = models.CharField(max_length=100)
-    sports = models.CharField(max_length=200)
-    required_act_score = models.CharField(max_length=2)
-    required_sat_score = models.CharField(max_length=4)
-    weather = models.CharField(max_length=10)
-    most_popular_major = models.CharField(max_length=25)
+class Institutions(models.Model):
+    institutionid = models.AutoField(db_column='InstitutionId', primary_key=True)  # Field name made lowercase.
+    unitid = models.CharField(db_column='UNITID', max_length=20, blank=True, null=True)  # Field name made lowercase.
+    opeid = models.CharField(db_column='OPEID', max_length=20, blank=True, null=True)  # Field name made lowercase.
+    opeid6 = models.CharField(db_column='OPEID6', max_length=20, blank=True, null=True)  # Field name made lowercase.
+    instname = models.CharField(db_column='InstName', max_length=200)  # Field name made lowercase.
+    accredagency = models.CharField(db_column='AccredAgency', max_length=200)  # Field name made lowercase.
+    insturl = models.CharField(db_column='InstURL', max_length=200, blank=True, null=True)  # Field name made lowercase.
+    pricecalcurl = models.CharField(db_column='PriceCalcURL', max_length=200, blank=True, null=True)  # Field name made lowercase.
+    statefips = models.CharField(db_column='StateFIPS', max_length=30, blank=True, null=True)  # Field name made lowercase.
+    maincampus = models.TextField(db_column='MainCampus', blank=True, null=True)  # Field name made lowercase. This field type is a guess.
+    numberofbranches = models.CharField(db_column='NumberOfBranches', max_length=2, blank=True, null=True)  # Field name made lowercase.
+    predominantdegrees = models.CharField(db_column='PredominantDegrees', max_length=1, blank=True, null=True)  # Field name made lowercase.
+    highestdegree = models.CharField(db_column='HighestDegree', max_length=1, blank=True, null=True)  # Field name made lowercase.
+    ownership = models.CharField(db_column='Ownership', max_length=1, blank=True, null=True)  # Field name made lowercase.
+    distanceonly = models.TextField(db_column='DistanceOnly', blank=True, null=True)  # Field name made lowercase. This field type is a guess.
+    latitude = models.FloatField(db_column='Latitude', blank=True, null=True)  # Field name made lowercase.
+    longitude = models.FloatField(db_column='Longitude', blank=True, null=True)  # Field name made lowercase.
+    locale = models.CharField(db_column='Locale', max_length=10, blank=True, null=True)  # Field name made lowercase.
+    zipcodeid = models.ForeignKey('Zipcodes', models.DO_NOTHING, db_column='ZipCodeId', blank=True, null=True)  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'Institutions'
 
     def __str__(self):
-        return self.name
+        return self.instname
 
+class Cities(models.Model):
+    cityid = models.AutoField(db_column='CityId', primary_key=True)  # Field name made lowercase.
+    city = models.CharField(db_column='City', max_length=100)  # Field name made lowercase.
+    state = models.CharField(db_column='State', max_length=2)  # Field name made lowercase.
+    region = models.CharField(db_column='Region', max_length=2)  # Field name made lowercase.
 
-class NearbyRestaurant(models.Model):
-    university = models.ForeignKey(University, on_delete=models.CASCADE)
-    restaurant_name = models.CharField(max_length=150)
-    restaurant_type = models.CharField(max_length=50)
-    restuarant_address = models.CharField(max_length=200, default="123 Address, St, Cincinnati, OH 45209")
-    restaurant_url = models.URLField(max_length=250, default='000000')
+    class Meta:
+        managed = False
+        db_table = 'Cities'
+    
+    def __str__(self):
+        return self.city + ", " + self.state
+
+class Zipcodes(models.Model):
+    zipcodeid = models.AutoField(db_column='ZipCodeId', primary_key=True)  # Field name made lowercase.
+    cityid = models.ForeignKey(Cities, models.DO_NOTHING, db_column='CityId')  # Field name made lowercase.
+    zipcode = models.CharField(db_column='ZipCode', max_length=10)  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'ZipCodes'
 
     def __str__(self):
-        return self.restaurant_name
+        return self.zipcode
