79d988007d9d335204ce23c66c6da0afd352463e
Tristan Palmer <palmertt@mail.uc.edu>
2020-10-26 19:46:01 -0400

    Added more models from database

--- universities/admin.py
+++ universities/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
-from .models import Institutions, Cities, Zipcodes
+from .models import Institutions, Cities, Zipcodes, Admissions, Completionrates, Costs, Institutiontypes, Majors, Programs, Undergraduates
 
 # Register your models here.
 admin.site.register(Institutions)
 admin.site.register(Cities)
 admin.site.register(Zipcodes)
+admin.site.register(Admissions)
+admin.site.register(Completionrates)
+admin.site.register(Costs)
+admin.site.register(Institutiontypes)
+admin.site.register(Majors)
+admin.site.register(Programs)
+admin.site.register(Undergraduates)
