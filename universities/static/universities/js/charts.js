5d9eeb360649b89774d88db87d57dd136c3caf25
Tristan Palmer <palmertt@mail.uc.edu>
2020-11-01 20:58:11 -0500

    Added admission charts

--- universities/static/universities/js/charts.js
+++ universities/static/universities/js/charts.js
@@ -30,3 +30,61 @@ let myTuitionChart = new Chart(ctx, {
         }
     }
 });
+
+let myActChart = new Chart(act, {
+    type: 'bar',
+    data: {
+        labels: ['Yellow'],
+        datasets: [{
+            label: 'Average ACT Score (out of 36)',
+            data: [parseInt(actScore)],
+            backgroundColor: [
+                'rgba(255, 206, 86, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 206, 86, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true,
+                    max : 40,
+                    min : 0
+                }
+            }]
+        }
+    }
+});
+
+let mySatChart = new Chart(sat, {
+    type: 'bar',
+    data: {
+        labels: ['Green'],
+        datasets: [{
+            label: 'SAT Score (out of 1600)',
+            data: [parseInt(satScore)],
+            backgroundColor: [
+                'rgba(75, 192, 192, 0.2)'
+            ],
+            borderColor: [
+                'rgba(75, 192, 192, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true,
+                    max : 1600,
+                    min : 0
+                }
+            }]
+        }
+    }
+});
