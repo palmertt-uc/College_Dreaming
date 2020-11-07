8bbd992257f8ff5c437a533d896ef1059063aa2a
devinwilliams <devinqwilliams13@gmail.com>
2020-11-07 12:44:53 -0500

    added IP address to allowed hosts

--- college_dreaming/settings.py
+++ college_dreaming/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '1o+h**91+@bor@p^!5+t(xl&r$t31_fcxy&^5b*_#6@@vq$%l)'
 DEBUG = True
 
 ALLOWED_HOSTS = ['college-dreaming-env.eba-4mmkhpe7.us-east-2.elasticbeanstalk.com', 'collegedreaming.com',
-                 'www.collegedreaming.com', '127.0.0.1', 'college-dreaming-application-env.eba-ygpnvbdy.us-east-2.elasticbeanstalk.com']
+                 'www.collegedreaming.com', '127.0.0.1', 'college-dreaming-application-env.eba-ygpnvbdy.us-east-2.elasticbeanstalk.com', '172.31.18.169']
 
 # Application definition
 
