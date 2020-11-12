11d351584c96872341a0367d3c3c311b7b398211
Tristan Palmer <palmertt@mail.uc.edu>
2020-11-12 10:34:44 -0500

    Added more filtering options and a collapse feature. Started work on the favorites feature

--- users/views.py
+++ users/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 from django.contrib.auth.decorators import login_required
+from universities.models import Institutions
+from django.http import HttpResponseRedirect
 
 
 # Create your views here.
