780592911d9cb28cc10f31b7bf07437d7293227e
Tristan Palmer <palmertt@mail.uc.edu>
2020-11-19 19:32:04 -0500

    Synced database and added tables to our models.py file

--- universities/models.py
+++ universities/models.py
@@ -45,6 +45,8 @@ class Cities(models.Model):
     city = models.CharField(db_column='City', max_length=100)  # Field name made lowercase.
     state = models.CharField(db_column='State', max_length=2)  # Field name made lowercase.
     region = models.CharField(db_column='Region', max_length=2)  # Field name made lowercase.
+    bars = models.IntegerField(db_column='Bars', blank=True, null=True)  # Field name made lowercase.
+    crimeid = models.ForeignKey('Crime', models.DO_NOTHING, db_column='CrimeId', blank=True, null=True)  # Field name made lowercase.
 
     class Meta:
         managed = False
@@ -75,27 +77,19 @@ class Institutions(models.Model):
     instname = models.CharField(db_column='InstName', max_length=200)  # Field name made lowercase.
     accredagency = models.CharField(db_column='AccredAgency', max_length=200)  # Field name made lowercase.
     insturl = models.CharField(db_column='InstURL', max_length=200, blank=True, null=True)  # Field name made lowercase.
-    pricecalcurl = models.CharField(db_column='PriceCalcURL', max_length=200, blank=True,
-                                    null=True)  # Field name made lowercase.
-    statefips = models.CharField(db_column='StateFIPS', max_length=30, blank=True,
-                                 null=True)  # Field name made lowercase.
-    maincampus = models.TextField(db_column='MainCampus', blank=True,
-                                  null=True)  # Field name made lowercase. This field type is a guess.
-    numberofbranches = models.CharField(db_column='NumberOfBranches', max_length=2, blank=True,
-                                        null=True)  # Field name made lowercase.
-    predominantdegrees = models.CharField(db_column='PredominantDegrees', max_length=1, blank=True,
-                                          null=True)  # Field name made lowercase.
-    highestdegree = models.CharField(db_column='HighestDegree', max_length=1, blank=True,
-                                     null=True)  # Field name made lowercase.
-    ownership = models.CharField(db_column='Ownership', max_length=1, blank=True,
-                                 null=True)  # Field name made lowercase.
-    distanceonly = models.TextField(db_column='DistanceOnly', blank=True,
-                                    null=True)  # Field name made lowercase. This field type is a guess.
+    pricecalcurl = models.CharField(db_column='PriceCalcURL', max_length=200, blank=True, null=True)  # Field name made lowercase.
+    statefips = models.CharField(db_column='StateFIPS', max_length=30, blank=True, null=True)  # Field name made lowercase.
+    maincampus = models.TextField(db_column='MainCampus', blank=True, null=True)  # Field name made lowercase. This field type is a guess.
+    numberofbranches = models.CharField(db_column='NumberOfBranches', max_length=2, blank=True, null=True)  # Field name made lowercase.
+    predominantdegrees = models.CharField(db_column='PredominantDegrees', max_length=1, blank=True, null=True)  # Field name made lowercase.
+    highestdegree = models.CharField(db_column='HighestDegree', max_length=1, blank=True, null=True)  # Field name made lowercase.
+    ownership = models.CharField(db_column='Ownership', max_length=1, blank=True, null=True)  # Field name made lowercase.
+    distanceonly = models.TextField(db_column='DistanceOnly', blank=True, null=True)  # Field name made lowercase. This field type is a guess.
     latitude = models.FloatField(db_column='Latitude', blank=True, null=True)  # Field name made lowercase.
     longitude = models.FloatField(db_column='Longitude', blank=True, null=True)  # Field name made lowercase.
     locale = models.CharField(db_column='Locale', max_length=10, blank=True, null=True)  # Field name made lowercase.
-    zipcodeid = models.ForeignKey(Zipcodes, models.DO_NOTHING, db_column='ZipCodeId', blank=True,
-                                  null=True)  # Field name made lowercase.
+    zipcodeid = models.ForeignKey('Zipcodes', models.DO_NOTHING, db_column='ZipCodeId', blank=True, null=True)  # Field name made lowercase.
+    climateid = models.ForeignKey(Climate, models.DO_NOTHING, db_column='ClimateId', blank=True, null=True)  # Field name made lowercase.
 
     class Meta:
         managed = False
