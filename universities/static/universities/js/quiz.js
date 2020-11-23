e668d745cf9f0aaea2f6322f64ddd57aa3efdcdf
Bond <bondgy@mail.uc.edu>
2020-11-22 20:14:37 -0500

    Added quiz functionality

--- universities/static/universities/js/quiz.js
+++ universities/static/universities/js/quiz.js
@@ -23,19 +23,47 @@
         });
     });
     $(".prefContainer").click(function () {
-        if ($(this).attr("data-isPref") === "true") {
+        if ($(this).attr("data-isPref") != "") {
+        alert("if");
             $(this).css({backgroundColor: '#6c757d'}, 250);
-            $(this).attr("data-isPref", "false");
+            let id = $(this).attr("id");
+            if(id === "contCrime"){
+                $(this).attr("data-isPref", "");
+            }else if(id === "contRestaurants"){
+                $(this).attr("data-isPref", "");
+            }else if(id === "contOutdoors"){
+                $(this).attr("data-isPref", "");
+            }else if(id === "contCommute"){
+                $(this).attr("data-isPref", "");
+            }else if(id === "contState"){
+                $(this).attr("data-isPref", "");
+            }else if(id === "contDiversity"){
+                $(this).attr("data-isPref", "");
+            }
+
         } else {
             $(this).css({backgroundColor: '#FFE066'}, 250);
-            $(this).attr("data-isPref", "true");
+            let id = $(this).attr("id");
+            if(id === "contCrime"){
+                $(this).attr("data-isPref", "ViolentCrime <= 300");
+            }else if(id === "contRestaurants"){
+                $(this).attr("data-isPref", "RestaurantRanking <= 50");
+            }else if(id === "contOutdoors"){
+                $(this).attr("data-isPref", "AvgTemp >= 60");
+            }else if(id === "contCommute"){
+                $(this).attr("data-isPref", "");
+            }else if(id === "contState"){
+                $(this).attr("data-isPref", "");
+            }else if(id === "contDiversity"){
+                $(this).attr("data-isPref", "demographics_white <= .7");
+            }
         }
     });
-    $().click(function () {
+    $("#preferencesStarterPrev").click(function () {
         if($("#preferencesStarter").css("display") != "none"){
             $("#preferencesStarter").fadeOut("slow", function () {
                 $("#formStarter").fadeIn(2000);
             });
         }
-    })
+    });
 });
