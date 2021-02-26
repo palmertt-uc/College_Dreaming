7589d0a6d5d3bb0529b74b1547c734a6f3f911ae
Tristan Palmer <palmertt@mail.uc.edu>
2021-02-25 21:24:03 -0500

    Added content to the bottom of the home page and edited contact.js file so that it only opens your default email app

--- universities/static/universities/js/contact.js
+++ universities/static/universities/js/contact.js
@@ -1,14 +1,11 @@
 // Contact Page information
 const sendEmail = document.getElementById('contact');
-const emailSender = document.getElementById('emailInput');
-const emailContent = document.getElementById('messageContent');
-const subject = document.getElementById('subject');
 const emailRecipient = 'seniordesign908@gmail.com'
 
 // Button For Sending Email
 sendEmail.addEventListener('click', function () {
 
-    let mailto_link = 'mailto:' + emailRecipient + '?subject=' + 'Email From '+ emailSender.value + ': ' + subject.value + '&body=' + emailContent.value;
+    let mailto_link = 'mailto:' + emailRecipient;
 
     window = window.open(mailto_link, 'emailWindow');
     if (window && window.open && !window.closed) {
