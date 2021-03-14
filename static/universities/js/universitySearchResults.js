92cbb5d6141ce4a02526dff66271cd59d3f2a614
Tristan Palmer <palmertt@mail.uc.edu>
2021-03-14 18:25:38 -0400

    Changed debug mode to false for production, collected static

---
+++ static/universities/js/universitySearchResults.js
@@ -0,0 +1,33 @@
+// New Design Code
+let open = document.getElementById('hamburger');
+let mainContent = document.getElementById('universities-search-results-content');
+let changeIcon = true;
+let windowSize = window.innerWidth
+
+open.addEventListener("click", function(){
+
+    let overlay = document.querySelector('.overlay');
+    let nav = document.querySelector('nav');
+    let icon = document.querySelector('.menu-toggle i');
+
+    overlay.classList.toggle("menu-open");
+    nav.classList.toggle("menu-open");
+    mainContent.classList.add("d-none");
+
+    if (windowSize > 800) {
+        mainContent.classList.remove("d-none");
+    }
+
+    if (changeIcon) {
+        icon.classList.remove("fa-bars");
+        icon.classList.add("fa-times");
+
+        changeIcon = false;
+    }
+    else {
+        icon.classList.remove("fa-times");
+        icon.classList.add("fa-bars");
+        mainContent.classList.remove("d-none");
+        changeIcon = true;
+    }
+});
