c36fd2008dac52ddcc130abbf1999942c5fabdec
Tristan Palmer <palmertt@mail.uc.edu>
2021-03-16 15:25:12 -0400

    Added search bar to larger screens

--- universities/static/universities/js/base.js
+++ universities/static/universities/js/base.js
@@ -46,10 +46,76 @@ function searchSuccess(data, textStatus, jqXHR) {
     }
 }
 
+function largeSearchColleges() {
+    searchTerm = document.getElementById("search-input");
+    let getUrl = window.location;
+    let baseUrl = getUrl .protocol + "//" + getUrl.host + "/"
+
+    $.ajax({
+        type: "GET",
+        url: baseUrl + "api/search-universities/",
+        data: {
+            'query' : $('#search-input').val(),
+        },
+        success: largeSearchSuccess,
+    });
+}
+
+function largeSearchSuccess(data, textStatus, jqXHR) {
+    if(data.length === 0 || document.getElementById("search-input").value.length === 0) {
+        document.getElementById("large-search-results").style.display = "none";
+    }else {
+        document.getElementById("large-search-results").style.display = "block";
+    }
+
+    document.getElementById("large-search-results-list").innerHTML = '';
+
+    for(let i = 0; i < data.length; i++) {
+        console.log(data[i]["instname"]);
+
+        let listItem = document.createElement("LI");
+        let listItemLink = document.createElement("a");
+        let listItemCityState = document.createElement("span")
+        let br = document.createElement("br");
+
+        // Create the text & link for anchor element.
+        let link = document.createTextNode(data[i]["instname"]);
+        listItemLink.appendChild(link);
+        listItemLink.href = "/universities/" + data[i]["institutionid"];
+
+        //Create the text for span element
+        let cityState = document.createTextNode(data[i]["zipcodeid"]["cityid"]['city'] + ', ' + data[i]["zipcodeid"]["cityid"]['state']);
+        listItemCityState.appendChild(cityState);
+
+        listItem.appendChild(listItemLink);
+        listItem.appendChild(listItemCityState);
+
+        document.getElementById("large-search-results-list").appendChild(listItem);
+    }
+}
+
 let searchResultList = document.getElementById('search-results');
+let searchPopup = document.querySelector('.search-popup');
+let brandName = document.querySelector('.brand-name');
 
 document.addEventListener('click', function (e) {
+    let largeSearchResultList = $('#large-search-results');
+
+    if (!largeSearchResultList.is(e.target) && largeSearchResultList.has(e.target).length === 0) {
+        largeSearchResultList.hide();
+    }
+
     if (!searchResultList.contains(e.target)) {
         searchResultList.style.display = 'none';
     }
 });
+
+function openSearchForm() {
+    if (searchPopup.style.display === 'block') {
+        searchPopup.style.display = 'none';
+        brandName.style.visibility = 'visible'
+    } else {
+        searchPopup.style.display = 'block';
+        brandName.style.visibility = 'hidden'
+    }
+}
